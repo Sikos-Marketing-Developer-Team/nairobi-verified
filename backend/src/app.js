1d76ea763c9111745416513fe996b36314b399f2
jxkimathi <judescriva8@gmail.com>
2025-03-24 20:11:55 +0300

    Update backend port to 5000 in order to accomodate the frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,4 @@
+// Main App
 require('dotenv').config({ path: './.env' });
 const express = require('express');
 const session = require('express-session');
@@ -30,7 +31,7 @@ app.use(passport.session());
 app.use('/api/auth', authRoutes);
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost/${PORT}`);
 });
