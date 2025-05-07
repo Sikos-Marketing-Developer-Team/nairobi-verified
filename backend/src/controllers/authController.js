56db3de9f81ee9955208cb36ea26f9d7d74e310b
Mark-andrew7 <kamaumark19@gmail.com>
2025-05-07 16:04:20 +0000

    Change cross site to none

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -86,7 +86,7 @@ const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'None',
       maxAge: rememberMe ? 30 * 24 * 60 * 60 * 1000 : 24 * 60 * 60 * 1000
     });
 
